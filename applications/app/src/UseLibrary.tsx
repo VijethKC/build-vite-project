103f7c6246066bee18effc8346be7092141c28db
VijethKC <vijethgowdakc@gmail.com>
2024-05-17 15:32:58 +0530

    vite monorepo

---
+++ applications/app/src/UseLibrary.tsx
@@ -0,0 +1,9 @@
+import { LIB_ONE } from '@vijeth/lib-one';
+
+const UseLibrary = ()=>{
+    return (
+        <div>Hey {LIB_ONE}</div>
+    )
+}
+
+export {UseLibrary}
---
+++ libraries/lib-one/src/index.ts
@@ -0,0 +1,5 @@
+const LIB_ONE = "LIBRARY ONE"
+
+export {
+    LIB_ONE
+}
